49a5bf0ef6e50ce2a6a7cd5126bc921c6c35f816
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2022-12-09 11:53:00 +0000

    ThingForm implementation

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
+from django import forms
+from .models import Thing
+
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    # name = forms.CharField(label='Your name')
+    # description = forms.Textarea(label='Description')
+    # quantity = forms.NumberInput(label='Quantity')
+    class Meta:
+        model = Thing
+        fields = ['name','description','quantity']
--- things/views.py
+++ things/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
+
+# def home(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = ThingForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             # return HttpResponseRedirect('/thanks/')
+
+#     # if a GET (or any other method) we'll create a blank form
+#     # else:
+#     #     form = ThingForm()
+
+#     return render(request, 'name.html', {'form': form})
